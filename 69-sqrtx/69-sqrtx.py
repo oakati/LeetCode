ad25be3e715629c11c71ff8a7547b972cca48ef1
oakati <101568377+oakati@users.noreply.github.com>
2022-06-15 15:25:47 +0300

    Time: 3229 ms (5.03%), Space: 13.4 MB (35.26%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        i = 0
+        while True:
+            square = i*i
+            if square > x:
+                return i - 1
+            i = i + 1
