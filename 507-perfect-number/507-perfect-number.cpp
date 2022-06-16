f49245f97dcb2cba777da51625bb5e1c9cabd3ab
oakati <101568377+oakati@users.noreply.github.com>
2022-06-16 20:44:58 +0300

    Time: 941 ms (50.00%), Space: 5.9 MB (21.27%) - LeetHub

---
+++ 507-perfect-number/507-perfect-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count = 0;
+        if(num % 2 == 1)
+        {
+            return false;
+        }
+        else if(num == 1)
+        {
+            return true;
+        }
+        else
+        {
+            for(int i = 1; i < num/2+1; i++)
+            {
+
+                if(num % i == 0)
+                {
+                    count += i;
+                    
+                }
+
+
+            }
+            return count == num;
+        }
+    }
+};
