b7c6d2a9f1418857e5f1f4c46c963558506b856f
oakati <101568377+oakati@users.noreply.github.com>
2022-06-25 01:49:03 +0300

    Time: 9 ms (13.45%), Space: 10.3 MB (85.74%) - LeetHub

--- 1037-valid-boomerang/1037-valid-boomerang.cpp
+++ 1037-valid-boomerang/1037-valid-boomerang.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool isBoomerang(vector<vector<int>>& points) {
-        cout << (points[0][0]-points[1][0]) << endl;
-        cout << (points[0][1]-points[1][1]) << endl;
-        cout << (points[1][0]-points[2][0]) << endl;
-        cout << (points[1][1]-points[2][1]) << endl;
         return !((points[0][1]-points[1][1])*(points[1][0]-points[2][0])==(points[0][0]-points[1][0])*(points[1][1]-points[2][1]));
     }
 };
