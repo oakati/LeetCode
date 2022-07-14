1427f619d83fad9ee4f1ebf79524488962866588
oakati <101568377+oakati@users.noreply.github.com>
2022-07-14 22:17:14 +0300

    Time: 165 ms (5.17%), Space: 5.6 MB (31.90%) - LeetHub

---
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 45
+int countBalls(int lowLimit, int highLimit){
+    int* array = malloc(SIZE*sizeof(int));
+    char text[7];
+    int sum, n, record;
+    for(int i = 0; i < SIZE; i++)
+    {
+        array[i] = 0;
+    }
+    for(int i = lowLimit; i <= highLimit; i++)
+    {
+        sum = 0;
+        n = sprintf(text, "%d", i);
+        for(int j = 0; j < n; j++)
+        {
+            sum += text[j]-48;
+        }
+        array[sum-1]++;
+    }
+    record = 0;
+    for(int i = 0; i < SIZE; i++)
+    {
+        if(array[i] > record)
+        {
+            record = array[i];
+        }
+    }
+    return record;
+}
