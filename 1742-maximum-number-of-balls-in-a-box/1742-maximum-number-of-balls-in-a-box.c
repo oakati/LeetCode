17dc895712da5ef89fee9e829644b6742d0c8640
oakati <101568377+oakati@users.noreply.github.com>
2022-07-14 22:25:41 +0300

    Time: 141 ms (5.17%), Space: 5.8 MB (8.62%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.c
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #define SIZE 45
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 int countBalls(int lowLimit, int highLimit){
     int* array = malloc(SIZE*sizeof(int));
-    char text[7];
-    int sum, n, record;
+    char* text = malloc(7*sizeof(char));
+    int sum, n, record = 0;
     for(int i = 0; i < SIZE; i++)
     {
         array[i] = 0;
     }
-    record = 0;
     for(int i = lowLimit; i <= highLimit; i++)
     {
-        sum = 0;
         n = sprintf(text, "%d", i);
-        for(int j = 0; j < n; j++)
+        sum = -48*n;
+        do
         {
-            sum += text[j];
-        }
-        sum -= 48*n;
+            sum += *text;
+            text++;
+        }while(*text);
+        text -= n;
         array[sum-1]++;
-        record = array[sum-1] > record ? array[sum-1] : record;
+        record = MAX(array[sum-1], record);
     }
     return record;
 }
