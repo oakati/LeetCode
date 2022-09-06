c54f6b432808a8ad5a77d9bdf1bd896f12637215
Ömer <101568377+oakati@users.noreply.github.com>
2022-09-06 18:48:36 +0300

    Time: 128 ms (46.15%), Space: 14 MB (16.26%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -7,10 +7,9 @@ class Solution(object):
         if len(s) == 1:
             return False
         m_list = [1]
-        for i in range(2, len(s)):
+        for i in range(2, int(ceil(len(s)/2))+1):
             if len(s) % i == 0:
                 m_list.append(i)
-        print(m_list)
         for i in m_list:
             if s.count(s[:i]) * i == len(s):
                 return True
