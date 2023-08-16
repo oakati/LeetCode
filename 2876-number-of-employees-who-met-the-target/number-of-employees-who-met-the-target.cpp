1dce22439ebb67b4643424274a04b5ea8e72d7bf
Ömer Katı <101568377+oakati@users.noreply.github.com>
2023-08-16 21:49:02 +0300

    Time: 9 ms (21.52%) | Memory: 20.5 MB (10.55%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int counter{0};
+        for(int i = 0; i < hours.size(); i++)
+        {
+            if(hours[i] >= target)
+                counter++;
+        }
+        return counter;
+    }
+};
